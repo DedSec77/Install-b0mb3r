e2d5a2b77b7271d0ddfc421a7123a8de95b72595
DedSec77 <55799553+DedSec77@users.noreply.github.com>
2019-09-25 18:16:39 +0300

    spofpskopkef
    sfesefuiosefyiyfopskopfse[ofiosefj09huesui

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo su
+apt update
+apt upgrade
+apt install python
+apt install python2
+apt install python3
+apt-get install python-pip
+apt-get install python3-pip
+clear
+echo "python done"
+apt-get install git
+clear 
+echo "git done"
+git clone https://github.com/crinny/b0mb3r
+cd b0mb3r
+pip3 install -r requirements.txt
+clear 
+echo "write command python3 main.py "
